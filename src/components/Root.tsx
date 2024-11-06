405143bd7d008eddb9306863c09c33f50978615a
hurst831028 <hurst831028@163.com>
2024-11-06 19:29:04 +0800

    Update Root.tsx

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,9 @@
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-import { App } from '@/components/App.tsx';
+//import { App } from '@/components/App.tsx';
 import { ErrorBoundary } from '@/components/ErrorBoundary.tsx';
 import { publicUrl } from '@/helpers/publicUrl.ts';
-//import { LayaAir } from './LayaAir';
+import { LayaAir } from './LayaAir';
 
 function ErrorBoundaryError({ error }: { error: unknown }) {
   return (
@@ -28,7 +28,7 @@ export function Root() {
       <TonConnectUIProvider
         manifestUrl={publicUrl('tonconnect-manifest.json')}
       >
-        <App/>
+        <LayaAir/>
       </TonConnectUIProvider>
     </ErrorBoundary>
   );
