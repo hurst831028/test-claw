7183d0de4598080d1896222ebc9640d5f18392b0
hurst831028 <hurst831028@163.com>
2024-11-07 19:54:36 +0800

    * update version

--- src/components/LayaAir.tsx
+++ src/components/LayaAir.tsx
@@ -1,5 +1,5 @@
 import { type FC, useRef, useEffect, useState } from 'react';  
-import { useLaunchParams, miniApp, useSignal, viewport, on } from '@telegram-apps/sdk-react';
+import { useLaunchParams, miniApp, useSignal, viewport } from '@telegram-apps/sdk-react';
 import { AppRoot} from '@telegram-apps/telegram-ui';
 //import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
 
@@ -35,24 +35,45 @@ export const LayaAir: FC = () => {
         //     }  
         // }, 100); 
         
-        //######
-        // const sendMessageToGame = (message: any) => {  
-        //     if (iframeRef.current && iframeRef.current.contentWindow && iframeRef.current.contentWindow.postMessage) {  
-        //         iframeRef.current.contentWindow.postMessage(message, '*'); // 第二个参数是目标源，'*'表示不限制源，但在生产环境中应该替换为允许的源列表  
-        //     }  
-        // };
+        const sendMessageToGame = (message: any) => {  
+            if (iframeRef.current && iframeRef.current.contentWindow && iframeRef.current.contentWindow.postMessage) {  
+                iframeRef.current.contentWindow.postMessage(message, '*'); // 第二个参数是目标源，'*'表示不限制源，但在生产环境中应该替换为允许的源列表  
+            }  
+        };
 
         // 设置消息监听器  
         function handleMessage(event: MessageEvent) {  
 
-            //if (event.origin !== 'http://your-layaair-game-origin.com') {  
-            //  return;  
-            //}  
+            // if (event.origin !== 'https://hurst831028.github.io/test-claw/') {  
+            //     console.log('GAME_LOG: not support!', event.origin);
+            //     return;  
+            // }  
         
             // 处理接收到的消息  
             const message = event.data;  
             // 根据消息类型执行相应的操作
             if (message.type === 'GAME_EVENT') {  
+                switch(message.data.handle) {
+                    case 'GAME_READY':
+                        //console.log('GAME_READY:', message);
+                        const message2 = { type: 'REACT_MESSAGE', 
+                            data: { 
+                                handle:"GAME_READY",
+                                "initDataRaw":lp.initDataRaw,
+                                "startParam":lp.startParam,
+                                "platform":lp.platform,
+                                "version":lp.version,
+                                // "theme":lp.themeParams.bg_color,
+                                // "color":lp.themeParams.text_color,
+                                // "lang":lp.themeParams.language,
+                            } 
+                        };
+                        sendMessageToGame(message2);
+                        break;
+                    default:
+                        break;
+                }
+
                 // 处理游戏事件  
                 // console.log('Received message from iframe:', message);  
                 // console.log('event.origin:', event.origin); 
@@ -61,21 +82,21 @@ export const LayaAir: FC = () => {
                 // sendMessageToGame(message2);  
 
             } else if (message.type === 'GAME_LOG') {
-                console.log('GAME_LOG:', message);
+                console.log('GAME_LOG:', message.data.message);
             }
         }  
         
         // 添加消息监听器到 window 对象  
         window.addEventListener('message', handleMessage, false);  
         
-        const removeListener = on('viewport_changed', payload => {
-            console.log('Viewport changed:', payload);
-        });
+        // const removeListener = on('viewport_changed', payload => {
+        //     //console.log('Viewport changed:', payload);
+        // });
 
         // 清理函数，用于组件卸载时移除事件监听器  
         return () => {  
             window.removeEventListener('message', handleMessage);  
-            removeListener();
+            //removeListener();
         };  
     }, []); 
 
